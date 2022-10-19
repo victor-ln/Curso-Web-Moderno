b0c4ca8aa7d24f1981b73e2f22a5c3fccac6d041
vlima-nu <vlima-nu@student.42sp.org.br>
2022-10-19 19:37:03 -0300

    fix: 🔨 Renaming file

--- fundamentals/varLetConst.js
+++ fundamentals/varLetConst.js
@@ -14,3 +14,23 @@ const c = 42;
 c += 15; <-- It fails!
 */
 console.log(c);
+
+/*
+    Variables defined with the 'var' keyword has global and function scope.
+*/
+var numberVar = 1;
+{
+    var numberVar = 2;
+    console.log("inside = ", numberVar);
+}
+console.log("outside = ", numberVar);
+
+/*
+    Variables defined with the 'let' keyword has global, block and function scope.
+*/
+let numberLet = 1;
+{
+    let numberLet = 2;
+    console.log("inside = ", numberLet);
+}
+console.log("outside = ", numberLet);
