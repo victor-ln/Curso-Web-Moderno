837276d6b25903ce058ecdaad1e0249ddcb5446a
vlima-nu <vlima-nu@student.42sp.org.br>
2022-10-25 16:28:34 -0300

    feat: 🚀 Adding file about IIFE in JS

---
+++ javascript/functions/09 - iife.js
@@ -0,0 +1,8 @@
+/**
+ * IIFE - Immediately Invoked Function Expression
+ */
+
+(function() {
+    console.log('Immediately executed');
+    console.log('Prevent to access external variables');
+})();
