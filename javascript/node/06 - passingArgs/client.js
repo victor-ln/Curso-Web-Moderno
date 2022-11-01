4ab217d9eab94953a5039d4dbaddeea998f5fba8
vlima-nu <vlima-nu@student.42sp.org.br>
2022-11-01 16:28:29 -0300

    feat: 🚀 passing arguments through modules in JS

---
+++ javascript/node/06 - passingArgs/client.js
@@ -0,0 +1,3 @@
+messages = require('./passingArgs')('James', 'Marvin', 'Stevie');
+
+console.log(messages);
---
+++ javascript/node/06 - passingArgs/passingArgs.js
@@ -0,0 +1,3 @@
+module.exports = function(...args) {
+    return args.map(name => `Have a good week ${name}`);
+}
