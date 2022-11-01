27d93170b0e3705b0ad77891201808139648557b
vlima-nu <vlima-nu@student.42sp.org.br>
2022-11-01 14:28:36 -0300

    docs: 💡 Adding comments about 'this' and 'exports'

--- javascript/node/00 - localModules/moduleA.js
+++ javascript/node/00 - localModules/moduleA.js
@@ -1,3 +1,15 @@
 this.message = 'Hello World!';
 exports.welcome = 'Welcome to node!';
 module.exports.seeya = 'See ya in the next example';
+
+/**
+ * 'this' and 'exports' are references to 'module.exports'.
+ * 
+ * Example:
+ */
+
+exports = { a: 1 }
+
+console.log(this);
+console.log(exports);
+console.log(module.exports);
